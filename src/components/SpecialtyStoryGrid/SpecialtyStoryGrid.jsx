4d0319ca6ea87bdf4e1fbe599e80a4ba7ac5a7c7
Derek Lim <dereklim3006@gmail.com>
2026-03-10 14:15:31 -0400

    feat(ex4): make specialty story grid responsive

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.jsx
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { MARKET_DATA, SPORTS_STORIES } from '../../data';
+import { QUERIES } from '../../constants';
 
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
@@ -47,14 +48,54 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    gap: 64px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr 1fr;
+    gap: 0;
+
+    > section:not(:last-of-type) {
+      border-right: 1px solid var(--color-gray-300);
+      padding-right: 16px;
+      margin-right: 16px;
+    }
+  }
 `;
 
 const MarketsSection = styled.section``;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 16px;
 
-const SportsSection = styled.section``;
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-columns: repeat(4, 1fr);
+  }
 
-const SportsStories = styled.div``;
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
+
+const SportsSection = styled.section`
+  min-width: 0;
+`;
+
+const SportsStories = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 16px;
+
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-columns: revert;
+    grid-auto-flow: column;
+    grid-auto-columns: 220px;
+    overflow-x: auto;
+  }
+`;
 
 export default SpecialtyStoryGrid;
